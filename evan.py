d417cf7ac8d3edc2aa6ee5dcd46aa41324c80509
Evan Williams <evan.a.williams82@gmail.com>
2014-02-09 18:57:01 -0500

    Updated to convert both the plaintext and the ciphertext into integer sums of their Ascii values, but I still couldn't figure out how to use Image.putdata() to create the images I began creating with Image.new().

--- evan.py
+++ evan.py
@@ -9,23 +9,40 @@
 # Licence:     Public Domain
 #-------------------------------------------------------------------------------
 
-import pygame, slowaes, sys, time, array
+import pygame, slowaes, sys, time
+from PIL import Image
 
 def gui():
     moo = slowaes.AESModeOfOperation()
     cleartext = raw_input("Enter a String to encrypt:")
     cypherkey = [143,194,34,208,145,203,230,143,177,246,97,206,145,92,255,84]
     iv = [103,35,148,239,76,213,47,118,255,222,123,176,106,134,98,92]
+
+    sum_ord = 0
+    for x in cleartext:
+        sum_ord += ord(x)
+    cleartext = str(sum_ord)
+
     mode, orig_len, ciph = moo.encrypt(cleartext, moo.modeOfOperation["CBC"],
-    		cypherkey, moo.aes.keySize["SIZE_128"], iv)
+            cypherkey, moo.aes.keySize["SIZE_128"], iv)
     print 'm=%s, ol=%s (%s), ciph=%s' % (mode, orig_len, len(cleartext), ciph)
     decr = moo.decrypt(ciph, orig_len, mode, cypherkey,
-    		moo.aes.keySize["SIZE_128"], iv)
+            moo.aes.keySize["SIZE_128"], iv)
     print decr
+
+    cleartext = int(cleartext)
+
+    clearData = []
+    for x in range(200):
+            clearData.append(cleartext)
+
+    ciphData = []
+    for x in range(200):
+            ciphData.append(ciph)
 
     pygame.init()
 
-    size = width, height = 320, 240
+    size = width, height = 400, 200
     speed = [2, 2]
     black = 0, 0, 0
 
@@ -36,23 +53,23 @@ def gui():
     ballrect = ball.get_rect()
 
     while True:
-    	for event in pygame.event.get():
-    		if event.type == pygame.QUIT: sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
 
-    	ballrect = ballrect.move(speed)
-    	if ballrect.left < 0 or ballrect.right > width:
-    		speed[0] = -speed[0]
-    	if ballrect.top < 0 or ballrect.bottom > height:
-    		speed[1] = -speed[1]
-        font = pygame.font.Font(None, 30)
-        strCIPH = "".join( chr( val ) for val in ciph )
-        textImg = font.render( "Ciphertext: " + strCIPH, 1, (255,40,0))
-        clearImg = font.render( "Cleartext: " + cleartext, 1, (0,255,40))
-    	screen.fill(black)
-    	screen.blit(ball, ballrect)
-        screen.blit(textImg, (0,0))
-        screen.blit(clearImg, (0,20))
-    	pygame.display.flip()
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+        clearImage = Image.new('RGB', (200,200))
+        clearImage.putdata(clearData)
+        ciphImage = Image.new('RGB', (200,200))
+        ciphImage.putdata(ciphData)
+        screen.fill(black)
+        screen.blit(ball, ballrect)
+        screen.blit(clearImage, (0,0))
+        screen.blit(ciphImage, (200,0))
+        pygame.display.flip()
         time.sleep(.01)
 
 if __name__ == '__main__':
