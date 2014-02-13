ca670b91eba1585c4d65fa57bc7ff157b7848f6d
Justin Warner <warner.73@wright.edu>
2014-02-13 18:08:30 -0500

    Adding a ToDo so other people can work on it.  Adding more View placeholders.

--- simplegui.py
+++ simplegui.py
@@ -6,6 +6,7 @@ import os
 
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import pygameui.resource as resource
 import pygameui as ui
 
 import logging
@@ -24,23 +25,9 @@ VSMALL_MARGIN = 1
 
 
 class KitchenSinkScene(ui.Scene):
-
-    def drawTopButtons(self):
-        topButtonNames = ["Edit SBox","RoundKeys","SubBytes", "ShiftRows","MixColumns","Run Round","Operation Modes"]
-        numButtons = len(topButtonNames)
-        buttonHeight = 20
-        self.buttonBarBottom = buttonHeight + MARGIN
-        buttonWidth = self.frame.w / numButtons
-        buttonX = (buttonWidth + VSMALL_MARGIN)
-        self.buttons = []
-        for i in range (0, numButtons):
-            btn = ui.Button(
-                ui.Rect(i * buttonX, 10, 0, buttonHeight),
-                topButtonNames[i])
-            btn.on_clicked.connect(self.processButtonClick)
-            self.add_child(btn)
-            
+    
     def editSBox(self):
+        self.addPointInHistory()
         print "Edit S Box"
     
     def runRoundKeys(self):
@@ -64,11 +51,11 @@ class KitchenSinkScene(ui.Scene):
     def updateMiddleColumn(self):
         print "Updating middle column"
     
-    def processButtonClick(self, btn, mbtn):
+    def onButtonClick(self, btn, mbtn):
         if btn.text == "Edit SBox":
             self.editSBox()
         elif btn.text == "RoundKeys":
-            self.runROundKeys()
+            self.runRoundKeys()
         elif btn.text == "SubBytes":
             self.runSubBytes()
         elif btn.text == "ShiftRows":
@@ -83,12 +70,58 @@ class KitchenSinkScene(ui.Scene):
             print "Don't know what to do"
         # We don't always have to update the middle column. But just so it's easy to read and less bull crap lines, 
         # we'll bbe updating it everytime this method is called.
-        updateMiddleColumn()
+        self.updateMiddleColumn()
         
         self.cleartext = self.cleartext + " J"
         self.mode, self.orig_len, self.ciph = self.moo.encrypt(self.cleartext, self.moo.modeOfOperation["CBC"],
                 self.cypherkey, self.moo.aes.keySize["SIZE_128"], self.iv)
         print self.ciph
+        
+    def stringToImage(self, string, view):
+        """ This method accepts a string, cipher/plain text (string), and updates the view (view) given. """
+        print string
+        
+    def updateLeftColumn(self):
+        """ This should update the left column """
+        print "Update left"
+    def updateRightColumn(self):
+        """ This should update the right column """
+        print "Update right"
+    def updateCenterColumn(self):
+        """ This should update the center column """
+        print "Update center"
+    
+        
+    def updateColumns(self):
+        """ This updates all the columns.  Redraws the left, right, and center. """
+        self.updateLeftColumn()
+        self.updateMiddleColumn()
+        self.updateRightColumn()
+        
+        
+    def onTextChanged(self, tf, text):
+        """ tf is the TextField object.  Text is the text the TextField was changed to. """
+        if tf.placeholder == "Plain Text One":
+            self.pto = text; # Plain Text One
+        elif tf.placeholder == "Plain Text Two":
+            self.ptt = text # Plain Text Two
+        else:
+            print "Don't know what to do..."
+            
+        self.updateColumns()
+        
+    def drawTopButtons(self):
+        topButtonNames = ["Edit SBox","RoundKeys","SubBytes", "ShiftRows","MixColumns","Run Round","Operation Modes"]
+        numButtons = len(topButtonNames)
+        buttonWidth = self.frame.w / numButtons
+        buttonX = (buttonWidth + VSMALL_MARGIN)
+        self.buttons = []
+        for i in range (0, numButtons):
+            btn = ui.Button(
+                ui.Rect(i * buttonX, 10, 0, 20),
+                topButtonNames[i])
+            btn.on_clicked.connect(self.onButtonClick)
+            self.add_child(btn)
         
     def drawLinesForGrid(self):
         """
@@ -99,12 +132,10 @@ class KitchenSinkScene(ui.Scene):
             and current progress.
         """
     
-        self.columnWidth = self.frame.w/3
-        self.verticalBarY = (self.frame.h - self.buttonBarBottom) / 2
-        
         # Draw bottom of top button bar.
-        slider = ui.SliderView(ui.Rect(
-            0, self.buttonBarBottom-2, self.frame.w, self.buttonBarBottom+2), ui.HORIZONTAL, 0, 1, False)
+        slider = ui.SliderView(
+            ui.Rect(0, self.buttonBarBottom-2, self.frame.w, self.buttonBarBottom+2),
+            ui.HORIZONTAL, 0, 1, False)
         slider.value = 1
         self.add_child(slider)
         
@@ -118,7 +149,7 @@ class KitchenSinkScene(ui.Scene):
         
         # Draw center bar.
         slider = ui.SliderView(
-            ui.Rect(0, self.verticalBarY-2, self.frame.w, 4), 
+            ui.Rect(0, self.middleBarY+(self.label_height/2), self.frame.w, 4), 
             ui.HORIZONTAL, 0, 1, False)
         slider.value = 1
         self.add_child(slider)
@@ -129,14 +160,25 @@ class KitchenSinkScene(ui.Scene):
             This column consists of two surfaces showing the initial string, 
             along input text boxes for the strings.
         """
+        self.plaintextone_imageview = ui.View(ui.Rect(0, self.buttonBarBottom+  MARGIN, self.columnWidth,self.middleBarY + MARGIN))
+        self.add_child(self.plaintextone_imageview)
         
-        frame = ui.Rect(10, self.buttonBarBottom, 200, self.label_height)
-        self.plaintextone_textfield = ui.TextField(frame, placeholder='Plain Text One')
+        self.plaintexttwo_imageview = ui.View(ui.Rect(0, self.middleBarY + MARGIN, self.columnWidth,self.frame.h))
+        self.add_child(self.plaintexttwo_imageview)
+        
+        # Draw these text fields at end so they're on top of the pictures.
+        self.plaintextone_textfield = ui.TextField(
+            ui.Rect(0, self.middleBarY-15, 200, self.label_height), 
+            placeholder='Plain Text One')
         self.plaintextone_textfield.centerx = self.frame.centerx
+        self.plaintextone_textfield.on_text_change.connect(self.onTextChanged)
         self.add_child(self.plaintextone_textfield)
-        frame = ui.Rect(10, self.buttonBarBottom, 200, self.label_height)
-        self.plaintexttwo_textfield = ui.TextField(frame, placeholder='Plain Text Two')
+        
+        self.plaintexttwo_textfield = ui.TextField(
+            ui.Rect(0, self.middleBarY+15, 200, self.label_height),
+            placeholder='Plain Text Two')
         self.plaintexttwo_textfield.centerx = self.frame.centerx
+        self.plaintexttwo_textfield.on_text_change.connect(self.onTextChanged)
         self.add_child(self.plaintexttwo_textfield)
         
     def drawRightColumn(self):
@@ -145,7 +187,12 @@ class KitchenSinkScene(ui.Scene):
             This will be the normal result of the plain texts if ran normally 
             through AES. 
         """
-        print "Drawing right"
+        self.cipherone_imageview = ui.View(ui.Rect(self.columnWidth*2, self.buttonBarBottom+  MARGIN, self.columnWidth,self.middleBarY + MARGIN))
+        self.add_child(self.cipherone_imageview)
+        
+        self.ciphertwo_imageview = ui.View(ui.Rect(self.columnWidth*2, self.middleBarY + MARGIN, self.columnWidth,self.frame.h))
+        self.add_child(self.ciphertwo_imageview)
+        
         
     def drawCenterColumn(self):
         """ 
@@ -153,7 +200,33 @@ class KitchenSinkScene(ui.Scene):
             This will be the current cipher keys and a slider at the bottom 
             for history. 
         """
-        print "Drawing center"
+        self.currentone_imageview = ui.View(ui.Rect(self.columnWidth, self.buttonBarBottom +  MARGIN, self.columnWidth,self.middleBarY + MARGIN))
+        self.add_child(self.currentone_imageview)
+        
+        self.currenttwo_imageview = ui.View(ui.Rect(self.columnWidth, self.middleBarY + MARGIN, self.columnWidth,self.frame.h))
+        self.add_child(self.currenttwo_imageview)
+        
+        
+        # Draw history slider
+        self.history = ui.SliderView(
+            ui.Rect(self.columnWidth + SMALL_MARGIN, self.frame.h-MARGIN, self.columnWidth-MARGIN, SMALL_MARGIN),
+            ui.HORIZONTAL, 0, 1, True)
+        self.history.value = 1
+        self.history.on_value_changed.connect(self.historyChanged)
+        self.add_child(self.history)
+        
+    def addPointInHistory(self):
+        # To do.
+        self.history.high = self.history.high + 1
+        self.history.value = self.history.high
+        print "Adding point in history"
+        
+    def resetHistory(self):
+        print "History reset"
+        
+    def historyChanged(self, slider_view, value):
+        # To do.
+        print "We're in history change mode! Now at: " + str(value)
         
     def __init__(self):
         ui.Scene.__init__(self)
@@ -170,7 +243,10 @@ class KitchenSinkScene(ui.Scene):
         size = width, height = int(infoObject.current_w), int(infoObject.current_h)
 
         frame = ui.Rect(MARGIN, MARGIN, width, height)
-
+        self.buttonBarBottom = self.label_height+VSMALL_MARGIN
+        self.columnWidth = self.frame.w/3
+        self.middleBarY = (self.frame.h - self.buttonBarBottom) / 2
+        
         self.drawTopButtons()
         self.drawLeftColumn()
         self.drawRightColumn()
