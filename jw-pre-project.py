60ee1bf8c8d1a224b81ed2444ac079847eeae348
Aaron <amon41amarth@gmail.com>
2014-03-06 17:53:39 -0500

    reformatted everything so i stop twitching

--- jw-pre-project.py
+++ jw-pre-project.py
@@ -1,72 +1,231 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This will create a dist directory containing the executable file, all the data
+# directories. All Libraries will be bundled in executable file.
 #
-# Author:      justinwarner
+# Run the build process by entering 'pygame2exe.py' or
+# 'python pygame2exe.py' in a console prompt.
 #
-# Created:     22/01/2014
-# Copyright:   (c) justinwarner 2014
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+# To build exe, python, pygame, and py2exe have to be installed. After
+# building exe none of this libraries are needed.
+# Please Note have a backup file in a different directory as if it crashes you
+# will loose it all!(I lost 6 months of work because I did not do this)
 
-import pygame, slowaes, sys, time, array
-from pygame.locals import *
-def gui():
-	moo = slowaes.AESModeOfOperation()
-	cleartext = ""
-	cypherkey = [143,194,34,208,145,203,230,143,177,246,97,206,145,92,255,84]
-	iv = [103,35,148,239,76,213,47,118,255,222,123,176,106,134,98,92]
-	mode, orig_len, ciph = moo.encrypt(cleartext, moo.modeOfOperation["CBC"],
-			cypherkey, moo.aes.keySize["SIZE_128"], iv)
-	pygame.init()
+try:
+    from distutils.core import setup
+    import py2exe
+    import pygame
+    from modulefinder import Module
+    import glob
+    import fnmatch
+    import sys
+    import os
+    import shutil
+    import operator
+except ImportError, message:
+    raise SystemExit, 'Unable to load module. %s' % message
 
-	black = 0, 0, 0
-	infoObject = pygame.display.Info()
-	size = width, height = int(infoObject.current_w/2), int(infoObject.current_h/2)
-	screen = pygame.display.set_mode((width, height))
+# hack which fixes the pygame mixer and pygame font
 
-	pygame.display.set_caption( 'AES Encrypt Mini Project' )
-	plainText = "";
-	imageSize = (width, int(height/2))
-	while True:
-		screen.fill(black)
-		didChange = False
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				didChange = True
-				if event.key == pygame.K_BACKSPACE:
-					plainText = plainText[:-1]
-				else:
-					plainText += event.unicode
-			elif event.type == pygame.QUIT:
-				sys.exit()
-			if didChange:
-				mode, orig_len, ciph = moo.encrypt(plainText, moo.modeOfOperation["CBC"], cypherkey, moo.aes.keySize["SIZE_128"], iv)
-		
-		cipherDraw = pygame.Surface((len(ciph), 1))
-		cipherDraw = cipherDraw.convert()
-		# Alter cipherDraw
-		run = 0
-		for pix in ciph:
-			cipherDraw.set_at((run, 0), (Color((pix),(pix),(pix),255)))
-			run+=1
-		for x in range (0, 4):
-			cipherDraw = pygame.transform.scale2x(cipherDraw)
-		screen.blit(cipherDraw, (0, height/2))
-		
-		plainDraw = pygame.Surface((len(plainText), 1))
-		plainDraw = plainDraw.convert()
-		# Alter plainDraw.
-		run = 0
-		plainText.rjust(len(ciph), '0') # Pad 0's for same length/look.
-		for pix in plainText:
-			plainDraw.set_at((run, 0), (Color(ord(pix),ord(pix),ord(pix),255)))
-			run+=1
-		for x in range (0, 4):
-			plainDraw = pygame.transform.scale2x(plainDraw)
-		screen.blit(plainDraw, (0, 0))
-		pygame.display.flip()
-		time.sleep(.1)
-		
+origIsSystemDLL = py2exe.build_exe.isSystemDLL  # save the orginal before we edit it
+
+
+def isSystemDLL(pathname):
+
+    # checks if the freetype and ogg dll files are being included
+
+    if os.path.basename(pathname).lower() in ('libfreetype-6.dll',
+            'libogg-0.dll', 'sdl_ttf.dll'):  # "sdl_ttf.dll" added by arit.
+        return 0
+    return origIsSystemDLL(pathname)  # return the orginal function
+
+
+py2exe.build_exe.isSystemDLL = isSystemDLL  # override the default function with this one
+
+
+class pygame2exe(py2exe.build_exe.py2exe):  # This hack make sure that pygame default font is copied: no need to modify code for specifying default font
+
+    def copy_extensions(self, extensions):
+
+        # Get pygame default font
+
+        pygamedir = os.path.split(pygame.base.__file__)[0]
+        pygame_default_font = os.path.join(pygamedir,
+                pygame.font.get_default_font())
+
+        # Add font to list of extension to be copied
+
+        extensions.append(Module('pygame.font', pygame_default_font))
+        py2exe.build_exe.py2exe.copy_extensions(self, extensions)
+
+
+class BuildExe:
+
+    def __init__(self):
+
+        # Name of starting .py
+
+        self.script = 'simplegui.py'
+
+        # Name of program
+
+        self.project_name = 'MyApps'
+
+        # Project url
+
+        self.project_url = 'about:none'
+
+        # Version of program
+
+        self.project_version = '0.0'
+
+        # License of the program
+
+        self.license = 'MyApps License'
+
+        # Auhor of program
+
+        self.author_name = 'Me'
+        self.author_email = 'example@example.com'
+        self.copyright = 'Copyright (c) 2009 Me.'
+
+        # Description
+
+        self.project_description = 'MyApps Description'
+
+        # Icon file (None will use pygame default icon)
+
+        self.icon_file = None
+
+        # Extra files/dirs copied to game
+
+        self.extra_datas = []
+
+        # Extra/excludes python modules
+
+        self.extra_modules = []
+        self.exclude_modules = []
+
+        # DLL Excludes
+
+        self.exclude_dll = ['']
+
+        # python scripts (strings) to be included, seperated by a comma
+
+        self.extra_scripts = []
+
+        # Zip file name (None will bundle files in exe instead of zip file)
+
+        self.zipfile_name = None
+
+        # Dist directory
+
+        self.dist_dir = 'dist'
+
+    # # Code from DistUtils tutorial at http://wiki.python.org/moin/Distutils/Tutorial
+    # # Originally borrowed from wxPython's setup and config files
+
+    def opj(self, *args):
+        path = os.path.join(*args)
+        return os.path.normpath(path)
+
+    def find_data_files(
+        self,
+        srcdir,
+        *wildcards,
+        **kw
+        ):
+
+        # get a list of all files under the srcdir matching wildcards,
+        # returned in a format to be used for install_data
+
+        def walk_helper(arg, dirname, files):
+            if '.svn' in dirname:
+                return
+            names = []
+            (lst, wildcards) = arg
+            for wc in wildcards:
+                wc_name = self.opj(dirname, wc)
+                for f in files:
+                    filename = self.opj(dirname, f)
+
+                    if fnmatch.fnmatch(filename, wc_name) \
+                        and not os.path.isdir(filename):
+                        names.append(filename)
+            if names:
+                lst.append((dirname, names))
+
+        file_list = []
+        recursive = kw.get('recursive', True)
+        if recursive:
+            os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+        else:
+            walk_helper((file_list, wildcards), srcdir,
+                        [os.path.basename(f) for f in
+                        glob.glob(self.opj(srcdir, '*'))])
+        return file_list
+
+    def run(self):
+        if os.path.isdir(self.dist_dir):  # Erase previous destination dir
+            shutil.rmtree(self.dist_dir)
+
+        # Use the default pygame icon, if none given
+
+        if self.icon_file == None:
+            path = os.path.split(pygame.__file__)[0]
+            self.icon_file = os.path.join(path, 'pygame.ico')
+
+        # List all data files to add
+
+        extra_datas = [
+            'empty.png',
+            'pygameui\\resources\\images\\info.png',
+            'pygameui\\resources\\images\\logo.png',
+            'pygameui\\resources\\images\\shadow.png',
+            'pygameui\\resources\\images\\spinner.png',
+            'pygameui\\resources\\images\\star.png',
+            ]
+        for data in self.extra_datas:
+            if os.path.isdir(data):
+                extra_datas.extend(self.find_data_files(data, '*'))
+            else:
+                extra_datas.append(('.', [data]))
+
+        setup(  # targets to build
+            cmdclass={'py2exe': pygame2exe},
+            version=self.project_version,
+            description=self.project_description,
+            name=self.project_name,
+            url=self.project_url,
+            author=self.author_name,
+            author_email=self.author_email,
+            license=self.license,
+            windows=[{'script': self.script, 'icon_resources': [(0,
+                     self.icon_file)], 'copyright': self.copyright}],
+            options={'py2exe': {
+                'optimize': 2,
+                'bundle_files': 1,
+                'compressed': True,
+                'excludes': self.exclude_modules,
+                'packages': self.extra_modules,
+                'dll_excludes': self.exclude_dll,
+                'includes': self.extra_scripts,
+                }},
+            zipfile=self.zipfile_name,
+            data_files=extra_datas,
+            dist_dir=self.dist_dir,
+            )
+
+        if os.path.isdir('build'):  # Clean up build dir
+            shutil.rmtree('build')
+
+
 if __name__ == '__main__':
-	gui()
+    if operator.lt(len(sys.argv), 2):
+        sys.argv.append('py2exe')
+    BuildExe().run()  # Run generation
+    raw_input('Press any key to continue')  # Pause to let user see that things ends
+
+
+			
